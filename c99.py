38db48182b2782fe2d38b4a097f230285d87d3bf
Shadowgamer483 <60564162+Shadowgamer483@users.noreply.github.com>
2021-07-21 14:47:41 +0200

    Add files via upload

---
+++ c99.py
@@ -0,0 +1,33 @@
+import os
+
+import shutil
+path=input("enter name of directry to be sorted master :)   ")
+listoffiles=os.listdir(path)
+for file in listoffiles:
+    name,ext=os.path.splitext(file)
+    ext=ext[1:]
+    if ext=="":
+        continue
+    if os.path.exists(path+"/"+ext):
+        shutil.move(path+"/"+file,path+"/"+ext+"/"+file)
+    else:
+        os.makedirs(path+"/"+ext)
+        shutil.move(path+"/"+file,path+"/"+ext+"/"+file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
